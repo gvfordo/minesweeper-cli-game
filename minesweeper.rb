3f6b344f5767526229bf3171f32b72a57851f80a
App Academy Student <student@appacademy.io>
2014-02-03 18:59:44 -0800

    Working Minesweeper game, colorization in progress

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,16 +1,18 @@
 # Require other classes
-
+require 'yaml'
+require 'colorize'
 class MineSweeper
 
   GAME_TEXT = {
     :action_question => "What would you like to do to that tile?",
     :coords_question => "Please enter the x,y coordinates separated by a space of the tile you want to change:",
     :invalid_input => "Invalid Input.",
-    :action_options => "1 = Reveal Tile  | 2 = Toggle Bomb Flag | 3 = Re-enter tile coordinates",
+    :action_options => "1 = Reveal Tile  | 2 = Toggle Bomb Flag | 3 = Re-enter tile coordinates | 4 = Save Game",
     :already_revealed => "This tile has already been revealed!",
     :end => "Game Over,  YOU LOOOOOOOOSE",
     :has_bomb_flag => "You must un-toggle the tile before it can be revealed",
-    :won => "You've won the game!"
+    :won => "You've won the game!",
+    :saved_game => "You saved the game!"
   }
 
   def initialize(player)
@@ -22,9 +24,9 @@ class MineSweeper
     @game_board.create_game
   end
 
-  def play_game
+  def play_game(saved = false)
     # Ask player what size they want.  #initialize_game(with_size)
-    initialize_game(9, 9)
+    initialize_game(20, 20) unless saved
     until game_over?
       puts @game_board
       coords = get_coords_from_player
@@ -38,6 +40,11 @@ class MineSweeper
         end
       elsif action == :toggle_flag
         @game_board.toggle_bomb_flag(coords)
+        system("clear")
+      elsif action == :save_game
+        save_game
+        puts GAME_TEXT[:saved_game]
+        break
       end
     end
 
@@ -64,13 +71,24 @@ class MineSweeper
     @player.get_coords
   end
 
+  def save_game
+    saved_game = self.to_yaml
 
+    File.open('save1.yaml', 'w'){ |file| file.puts saved_game }
+
+  end
+
+  def load_game
+
+  end
 end
 
 class Board
   attr_reader :board
 
-  def initialize(size, number_of_bombs = 10)
+
+
+  def initialize(size, number_of_bombs = 20)
     @size_x, @size_y = size
     @number_of_bombs = number_of_bombs
   end
@@ -83,16 +101,16 @@ class Board
       end
     end
     tiles.all? do |tile|
-      (tile.has_bomb? && !tile.revealed) || (!tile.has_bomb? && tile.revealed)
+      (tile.has_bomb? ^ tile.revealed)
     end
   end
 
   def lost?
     @board.each do |rows|
       rows.each do |col|
-          if col.revealed && col.has_bomb?
-            return true
-          end
+        if col.revealed && col.has_bomb?
+          return true
+        end
       end
     end
     false
@@ -148,7 +166,6 @@ class Board
     end
 
     if tile.adjacent_bombs == 0
-        # @board.explore_selection(coord)
       tiles = get_adjacents(coord)
       tiles.each do |tile|
         reveal(tile.pos)
@@ -182,23 +199,12 @@ class Board
 
   def number_of_adjacent_bombs(adjacents)
     adjacents.count(&:has_bomb?)
-    # bombs = adjacents.select do |adjacent|
-    #         x, y = adjacent[0], adjacent[1]
-    #         @board[y][x].has_bomb?
-    #         end
-    # bombs.count
   end
 
   def toggle_bomb_flag(coord)
     x, y = coord[0], coord[1]
     tile = @board[y][x]
     tile.has_bomb_flag = !tile.has_bomb_flag
-
-    # if @board[y][x].has_bomb_flag
-    #   @board[y][x].has_bomb_flag = false
-    # else
-    #   @board[y][x].has_bomb_flag = true
-    # end
   end
 
   def get_adjacents(coord)
@@ -211,7 +217,7 @@ class Board
     end
 
     adjacents_to_check = possible_array.select do |array|
-      (0..8).include?(array[0]) && (0..8).include?(array[1])
+      (0...@size_x).include?(array[0]) && (0...@size_y).include?(array[1])
     end
 
     adjacents_to_check.map do |x, y|
@@ -225,6 +231,7 @@ class Board
       row.each do |col|
         string += col.to_s
       end
+      string.strip!
       string += "\n"
     end
     string
@@ -235,6 +242,8 @@ end
 class Tile
   attr_accessor :display_value, :has_bomb_flag, :revealed, :adjacents, :has_bomb, :adjacent_bombs
   attr_reader :pos
+
+
 
   def initialize(pos, grid)
     @pos = pos  #Array pair ?  [0, 0]
@@ -251,53 +260,40 @@ class Tile
 
   def to_s
     if @has_bomb_flag
-      "F "
+      "F ".colorize(:light_red)
     elsif @revealed
-      @adjacent_bombs == 0 ? "  " : "#{@adjacent_bombs} "
-    # elsif @has_bomb
-#       "B "
+      @adjacent_bombs == 0 ? "  ".colorize(:background => :light_cyan) : "#{@adjacent_bombs} ".colorize(BOMB_COUNT_COLOR[@adjacent_bombs]).colorize(:background => :blue)
     else
-      "@ "
+      "  ".colorize(:light_white).colorize(:background => :light_white)
     end
   end
 
-  # def reveal
-  #   if @revealed
-  #     return 0
-  #   elsif @has_bomb_flag
-  #     return 1
-  #   else
-  #     @revealed = true
-  #     @adjacents = get_number_of_adjacent_bombs
-  #   end
-  # end
-  #
-  # def toggle_bomb_flag
-  #   if @has_bomb_flag
-  #     @has_bomb_flag = false
-  #   else
-  #     @has_bomb_flag = true
-  #   end
-  # end
-  #
-  # def get_number_of_adjacent_bombs
-  #   " "
-  #
-  #   possible_adjacents = [[1,1], [0,1], [-1,0], [-1,-1], [0, -1], [-1, 1], [1, -1], [1,0]]
-  #
-  #   possible_array = possible_adjacants.map do |array|
-  #     [@pos[0] + array[0], @pos[1] + array[1]]
-  #   end
-  #
-  #   adjacents_to_check = possible_array.select do |array|
-  #     (0..8).include?(array[0]) && (0..8).include?(array[1])
-  #   end
-  #
-  #   adjacents_to_check.each do |adjacent_tile|
-  #
-  #   end
-  #
-  # end
+  BOMB_COUNT_COLOR = {
+    0 => :white,
+    1 => :green,
+    2 => :blue,
+    3 => :magenta,
+    4 => :red
+
+    # :black,
+    #  :red,
+    #  :green,
+    #  :yellow,
+    #  :blue,
+    #  :magenta,
+    #  :cyan,
+    #  :white,
+    #  :default,
+    #  :light_black,
+    #  :light_red,
+    #  :light_green,
+    #  :light_yellow,
+    #  :light_blue,
+    #  :light_magenta,
+    #  :light_cyan,
+    #  :light_white
+  }
+
 end
 
 
@@ -319,11 +315,19 @@ class Player
       :reveal
     elsif input == "2"
       :toggle_flag
+    elsif input == "4"
+      :save_game
     end
   end
 end
 
 player = Player.new("Kevin")
-game = MineSweeper.new(player)
 
-game.play_game
+
+#game = MineSweeper.new(player)
+saved_game = File.read('save1.yaml')
+p saved_game[0..50]
+
+game = YAML::load(saved_game)
+
+game.play_game(false)
